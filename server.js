6dd49964be1a41afb3cb73326c4303e81ff31112
Gary Chin <garychin100@gmail.com>
2017-10-28 02:22:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+//middleware to define static files/images
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`);
+});
